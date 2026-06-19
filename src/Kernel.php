ca13c7c2c216654f2113229ae81353449d723dab
jprud67 <jprud67@gmail.com>
2026-06-19 12:33:10 +0100

    feat(flow): add NodeRegistry with auto-discovery compiler pass

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,9 +3,15 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new \App\DependencyInjection\FlowNodeHandlerPass());
+    }
 }
